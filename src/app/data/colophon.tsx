846d8ebf24f4e28d37ab417c28863eaba90749bc
Joe <joeymigs@gmail.com>
2025-03-14 18:00:29 -0600

    Release v3.0.0

--- src/app/data/colophon.tsx
+++ src/app/data/colophon.tsx
@@ -2,7 +2,7 @@ import NextJsSvg from '@/assets/icons/dev/nextjs.svg'
 import VercelSvg from '@/assets/icons/dev/vercel.svg'
 import ReactSvg from '@/assets/icons/dev/react.svg'
 import MotionSvg from '@/assets/icons/dev/motion.svg'
-import TypeScript from '@/assets/icons/dev/typeScript.svg'
+import TypeScript from '@/assets/icons/dev/typescript.svg'
 
 type Colophon = {
   name: string
